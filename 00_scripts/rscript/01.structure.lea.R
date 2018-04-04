046c02b4a8578db0984a3dc06c9d235e4117a41a
ubuntu <quentinrougemont@orange.fr>
2018-04-04 04:19:54 -0400

    changed models

--- 00_scripts/rscript/01.structure.lea.R
+++ 00_scripts/rscript/01.structure.lea.R
@@ -17,29 +17,16 @@ output = vcf2geno(input,"04_lea/__mode__.slim.__NB__/slim.__NB__.genotype.geno")
 obj.at = snmf("./04_lea/__mode__.slim.__NB__/slim.__NB__.genotype.geno", K = 3, ploidy = 2, entropy = T,repetitions=10,alpha=10,percentage=0.05,
               CPU = 12, project = "new")
 
-#pdf(file="CV.full.data.pdf",12,8)
-#plot(obj.at, col = "blue4", cex = 1.4, pch = 19)
-#dev.off()
 pop <- as.matrix((read.table("../../01_info_file/strata.txt")[,2] )) #pop names
 ce = cross.entropy(obj.at, K = 3)
 best = which.min(ce)
 
 qmatrix = Q(obj.at, K = 3, best)
 
-#K = ncol(qmatrix)
-#Npop <-  length(unique(pop))
 
 b=aggregate(qmatrix,list(pop),mean) 
 write.table(b, paste("lea.__mode__slim.__NB__.mean.txt",sep="."), 
 	quote=F, 
 	row.names=F, 
 	col.names=F)
-
-
-#tmp1<- mapply(rowsum, as.data.frame(qmatrix), as.data.frame(pop))
-#dimnames(tmp1)<- list(levels(factor(pop[,c(1)]))) #, 1:nrow(samp.siz.1[,c(1)]))
-#tmp<-as.matrix(table(pop))
-#tmp=rep(cbind(tmp),ncol(qmatrix))
-#qpop<-tmp1/tmp
-
 
